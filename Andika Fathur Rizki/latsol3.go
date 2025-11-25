d7e606ec7feadd5d9d59e203d80d036053e298ad
Andika-fathur <dikathur@gmail.com>
2025-11-25 21:10:04 +0700

    Add files via upload

---
+++ Andika Fathur Rizki/latsol3.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var b int
+	fmt.Print("Bilangan: ")
+	fmt.Scan(&b)
+
+	fmt.Print("Faktor: ")
+
+	jumlahFaktor := 0
+
+	for f := 1; f <= b; f++ {
+		if b%f == 0 {
+			fmt.Print(f, " ")
+			jumlahFaktor++
+		}
+	}
+	prima := false
+	if jumlahFaktor == 2 {
+		prima = true
+	}
+
+	fmt.Println()
+	fmt.Println("Prima:", prima)
+}
