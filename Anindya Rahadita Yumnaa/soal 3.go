4054b5e02fc85b71997694abb087d2f4da242553
anindyarahadita <yumnaaanindya@gmail.com>
2025-11-25 21:53:14 +0700

    Add files via upload

---
+++ Anindya Rahadita Yumnaa/soal 3.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    var b int
+    fmt.Print("Bilangan: ")
+    fmt.Scan(&b)
+
+    fmt.Print("Faktor: ")
+
+    jumlahFaktor := 0
+    for i := 1; i <= b; i++ {
+        if b%i == 0 {
+            fmt.Print(i, " ")
+            jumlahFaktor++
+        }
+    }
+    fmt.Println()
+    var prima bool
+    if jumlahFaktor == 2 {
+        prima = true
+    } else {
+        prima = false
+    }
+
+    fmt.Println("Prima:", prima)
+}
